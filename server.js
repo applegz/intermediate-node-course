d4d31aa140e438e36f77acbe827912e4fc438f96
Guoying Zhong <ffamochong@gmail.com>
2021-02-18 22:18:27 -0500

    Add user model and connect to db

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
-const express= require('express');
-const mongoose= require('mongoose');
-const bodyParser= require('body-parser');
-const port=8000;
-const app= express();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const User = require('./models/User');
+mongoose.connect('mongodb://localhost/userData');
+const port = 8000;
+const app = express();
 
 app.use(bodyParser.json());
 
-app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is listening on port:${port}`);
+});
 
 // CREATE
-app.post('/users',(req,res)=>{
+app.post('/users', (req, res) => {
   // User.create()
-})
+});
 
-app.route('/users/:id')
-// READ
-.get((req,res)=>{
-  // User.findById()
-})
-// UPDATE
-.put((req,res)=>{
-  // User.findByIdAndUpdate()
-})
-// DELETE
-.delete((req,res)=>{
-  // User.findByIdAndDelete()
-})
+app
+  .route('/users/:id')
+  // READ
+  .get((req, res) => {
+    // User.findById()
+  })
+  // UPDATE
+  .put((req, res) => {
+    // User.findByIdAndUpdate()
+  })
+  // DELETE
+  .delete((req, res) => {
+    // User.findByIdAndDelete()
+  });
